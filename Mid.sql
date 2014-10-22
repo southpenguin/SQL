e59a9b372558e62a4839b33c8bc9ac87d984459f
southpenguin <southpenguin@gmail.com>
2014-10-22 15:15:47 -0400

    Update Mid.sql

--- Mid.sql
+++ Mid.sql
@@ -1,2 +1,5 @@
-select name
-from 
+select P1.name
+from Physicist P1 join Authors A1 join Paper, Cites, Physicist P2 join Authors A2
+where A1.papid = Cites.citeid and
+  A2.papid = Cites.citedid and
+  P2.name = 'Albert Einstein';
