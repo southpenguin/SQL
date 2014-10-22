1e9164eb09c17fca04fe2c55ab0ea175e6acf2c0
southpenguin <southpenguin@gmail.com>
2014-10-22 15:30:31 -0400

    Update Mid.sql

--- Mid.sql
+++ Mid.sql
@@ -3,3 +3,14 @@ from Physicist P1 join Authors A1 join Paper, Cites, Physicist P2 join Authors A
 where A1.papid = Cites.citeid and
   A2.papid = Cites.citedid and
   P2.name = 'Albert Einstein';
+
+
+select name
+from Physicist join Authors A1, Authors A2
+where A1.papid = A2.papid and
+  Physicist.advisor = A2.pid
+group by Physicist.pid
+having count(distinct A1.papid) = 
+  (select count(distinct papid) 
+  from Authors A3 
+  where A3.pid = Physicist.pid)
