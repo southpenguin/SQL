9f0fc0b5eaed2ddc06b7eaf1f07da2b38b48065e
southpenguin <southpenguin@gmail.com>
2014-11-16 23:11:46 -0500

    Create index.php

---
+++ Sandwich/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+
+<form action = "get-input-example.php" method="POST">
+Please Enter a key word.<br>
+<input type="text" name="topic" placeholder="Key Word"><br>
+Please enter your phone number: <br>
+<input type="text" name="phone" placeholder="Phone"><br><br/>
+<input type = "submit" value = "Show books">
+</form>
+</body>
+</html>
