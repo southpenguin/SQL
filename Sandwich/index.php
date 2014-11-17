a3019a55f5aa2e7b06ed43354435088a85258863
southpenguin <southpenguin@gmail.com>
2014-11-17 02:57:34 -0500

    Update index.php

--- Sandwich/index.php
+++ Sandwich/index.php
@@ -2,13 +2,16 @@
 
 <html>
 <body>
-
-<form action = "get-input-example.php" method="POST">
-Please Enter a key word.<br>
-<input type="text" name="topic" placeholder="Key Word"><br>
-Please enter your phone number: <br>
-<input type="text" name="phone" placeholder="Phone"><br><br/>
-<input type = "submit" value = "Show books">
-</form>
+    <div style="width: 500px; margin-left: auto; margin-right: auto; text-align: center; font-family: 'lucida grande', Calibri, Arial, sans-serif;">
+        <h2>Welcome to our sandwich shop!</h2>
+        <h3>Please type the keyword and your phone number.</h3>
+        <form action = "placeOrder.php" method="POST">
+        <div style="width: 300px; margin-left: auto; margin-right: auto; font-size: 18px;">
+            <input type="text" name="keyword" placeholder="Key Word" style="font-weight: bold; height: 22px; font-size: 14px; width: 200px;"><br><br>
+            <input type="text" name="phone" placeholder="Your Phone Number" style="font-weight: bold; height: 22px; font-size: 14px; width: 200px;"><br><br>
+            <input type = "submit" value = "Create an Oeder">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
