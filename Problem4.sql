c54bb52f9d81e8cde925478cde2aa3f49af3390d
southpenguin <southpenguin@gmail.com>
2014-11-18 21:17:14 -0500

    Update Problem4.sql

--- Problem4.sql
+++ Problem4.sql
@@ -25,9 +25,28 @@ END
 
 
 
-SELECT COUNT(*) from information_schema.tables 
-WHERE table_type = 'base table' 
 
-SELECT TABLE_NAME, COUNT(*) 
+SELECT 
+  (SELECT COUNT(*) 
+  FROM information_schema.tables 
+  WHERE table_schema = 'HW2' ) 'Number of Tables',
+  (SELECT COUNT(*)
+  FROM INFORMATION_SCHEMA.COLUMNS 
+  WHERE table_schema = 'HW2') 'Number of Attributes'
+  
+  
+SELECT column_name 'Key Name', table_name 'Table', referenced_table_name 'Referenced Table', referenced_column_name 'Referenced Attribute'
+FROM information_schema.key_column_usage
+WHERE table_schema='HW2'
+  AND referenced_table_schema IS NOT NULL
+  
+  
+SELECT table_name Table, column_name Attribute
 FROM INFORMATION_SCHEMA.COLUMNS 
-GROUP BY TABLE_NAME
+WHERE table_schema = 'HW2'
+  AND column_name LIKE '%name%'
+  
+SELECT table_name, column_name
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE table_schema = 'HW2'
+  AND column_name LIKE '%name%'
